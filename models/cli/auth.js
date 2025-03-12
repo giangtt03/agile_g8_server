0aab91d07b8ce65759f15399a039e6b9e2231eab
giangtt03 <giangtt653@gmail.com>
2025-03-12 12:54:09 +0700

    first commit

---
+++ models/cli/auth.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model('TKNguoiDung', UserSchema);
